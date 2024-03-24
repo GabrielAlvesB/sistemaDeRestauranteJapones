e0b02bd25b9b81e36ab9e58b3e2c498a3052feef
GabrielAlvesB <gabrielalveskos@gmail.com>
2024-03-24 17:37:22 -0300

    Lista de produtos

--- src/components/cart/sidebar.tsx
+++ src/components/cart/sidebar.tsx
@@ -7,7 +7,7 @@ import {Separator} from "@/components/ui/separator";
   export const CartSidebar = () => {
     return (
         <Sheet>
-            <SheetTrigger>
+            <SheetTrigger asChild>
                 <Button>
                     <RocketIcon className="mr-2"/>
                     <p>Carrinho</p>
@@ -21,7 +21,18 @@ import {Separator} from "@/components/ui/separator";
                 <div className="flex flex-col gap-5 my-3">
 
                 </div>
-                <Separator clasName="my-4" />
+                <Separator  className="my-4" />
+
+                <div className="flex justify-between items-center text-xs">
+                    <div>Subtotal:</div>
+                    <div>...</div>
+                </div>
+
+                <Separator  className="my-4" />
+
+                <div className="text-center">
+                    <Button>Finalizar Compra</Button>
+                </div>
             </SheetContent>
         </Sheet>
     )
